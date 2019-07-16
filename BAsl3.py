9c483fe1a0f86294745b11a00e0c0c12a4bdabad
kashbari <admin@kashbari.com>
2019-07-16 12:00:54 -0500

    BAsl3.py modified from .sage. Computes all HWV in Grassmannian, but still need to verify rank of annihilating plane

--- BAsl3.py
+++ BAsl3.py
@@ -1,5 +1,12 @@
 import BorderApolarity
 import PosetHWV
+
+import numpy as np
+import sympy as sp
+import queue,itertools,math
+from scipy.sparse import csr_matrix, vstack, hstack
+from collections import deque
+
 import sys
 from sage.all import *
 
@@ -38,14 +45,14 @@ V11 = csr_matrix((data,(row,col)),shape=(n**4,1))
 
 
 
-LowOps = [SparseMatLowOp(i,n) for i in range(n-1)]
+LowOps = [BorderApolarity.SparseMatLowOp(i,n) for i in range(n-1)]
 C = CartanMatrix(['A',n-1])
 Cinv = np.linalg.inv(C)
 f = lambda x,y: all(i >= 0 for i in map(int,round(Cinv.dot(np.subtract(y,x)))))
 
 
 def WeightPoset(V,LowOps,f):
-	L = LatticeElts(V,LowOps)
+	L = BorderApolarity.LatticeElts(V,LowOps)
 	E = L.keys()
 	P = Poset((E,f))
 	LE = list(P.linear_extension())
@@ -53,15 +60,15 @@ def WeightPoset(V,LowOps,f):
 	N = [L[s].shape[1] for s in LE]
 	return L,LE,P,N
 
-L22,LE22,P22,N22 = WeightPoset(V22,LowOps,f)
+Dict22,LE22,P22,N22 = WeightPoset(V22,LowOps,f)
 
-L30,LE30,P30,N30 = WeightPoset(V30,LowOps,f)
+Dict30,LE30,P30,N30 = WeightPoset(V30,LowOps,f)
 
-L03,LE03,P03,N03 = WeightPoset(V03,LowOps,f)
+Dict03,LE03,P03,N03 = WeightPoset(V03,LowOps,f)
 
-L00,LE00,P00,N00 = WeightPoset(V00,LowOps,f)
+Dict00,LE00,P00,N00 = WeightPoset(V00,LowOps,f)
 
-L11,LE11,P11,N11 = WeightPoset(V11,LowOps,f)
+Dict11,LE11,P11,N11 = WeightPoset(V11,LowOps,f)
 
 #precomputed
 DIMKER = {}
@@ -69,16 +76,20 @@ DIMKER[(0,)] = 35
 DIMKER[(1,)] = 35
 DIMKER[(0,1)] = 9
 
+C1 = [len(LE22),len(LE30),len(LE03),len(LE00),len(LE11)]
+C = [c for c in BorderApolarity.findComp(p,5) if all( i >= 0 for i in np.subtract(C1,c))]
 
 
-C = [c for c in findComp(p,5)]
-
-
-def AnnPlane(c,LE22,P22,N22,LE30,P30,N30,LE03,P03,N03,LE00,P00,N00,LE11,P11,N11,DIMKER):
-	H22=dfs(c[0],LE22,N22,P22,DIMKER)
-	H30=dfs(c[1],LE30,N30,P30,DIMKER)
-	H03=dfs(c[2],LE03,N03,P03,DIMKER)
-	H00=dfs(c[3],LE00,N00,P00,DIMKER)
-	H11=dfs(c[4],LE11,N11,P11,DIMKER)
+def HWVGrassmannian(c):
+	H22=PosetHWV.dfs(c[0],LE22,N22,P22,DIMKER)
+	H30=PosetHWV.dfs(c[1],LE30,N30,P30,DIMKER)
+	H03=PosetHWV.dfs(c[2],LE03,N03,P03,DIMKER)
+	H00=PosetHWV.dfs(c[3],LE00,N00,P00,DIMKER)
+	H11=PosetHWV.dfs(c[4],LE11,N11,P11,DIMKER)
 	return H22,H30,H03,H00,H11
 
+'''
+def AnnPlane(K22,K30,K03,K00,K11):
+	AP = []
+	for
+''' 
--- PosetHWV.py
+++ PosetHWV.py
@@ -4,6 +4,7 @@ import queue,itertools,math
 import scipy.sparse
 from scipy.sparse import csr_matrix,vstack,hstack
 import BorderApolarity
+from collections import deque
 
 #L = LatticeElts(V,LowOps)
 #E = L.keys()
@@ -35,7 +36,7 @@ def dimker(LowOps,n):
 def subsets(s):
 	l = []
 	for i in range(1,len(s)+1): 
-		l += list(map(list,itertools.combinations(s,i)))
+		l += list(map(tuple,itertools.combinations(s,i)))
 	return l
 
  
@@ -43,6 +44,7 @@ def subsets(s):
 #dimker = {Lowering ops intersections as tuple:dimker}
 # K = [k_1, \cdots, k_l], change k_i so check conditions for k_i
 #Lin is linear extension, N is maximal elts, P is poset and DIMKER is dimensions of all kernels
+'''
 def conditions(K,LE,N,P,DIMKER):
 	Test = True
 	while Test != False:
@@ -52,7 +54,8 @@ def conditions(K,LE,N,P,DIMKER):
 					parents = P.upper_covers(LE[j])
 					if len(parents) != 0:
 						pi = [LE.index(p) for p in parents]
-						lo = [np.subtract(p,LE[i]).tolist().index(2) for p in parents]
+						lo = [np.subtract(p,LE[j]).tolist().index(2) for p in parents]
+						lo.sort()
 						for s in subsets(lo):
 							sum = DIMKER[s]
 							for i in s:
@@ -64,11 +67,58 @@ def conditions(K,LE,N,P,DIMKER):
 		else:
 			Test = False
 	return Test
+'''
 
+def cond(K,i,LE,P,DIMKER):
+	Test = True
+	parents = P.upper_covers(LE[i])
+	if len(parents) != 0:
+		pi = [LE.index(p) for p in parents]
+		lo = [np.subtract(p,LE[i]).tolist().index(2) for p in parents]
+		lo.sort()
+		for s in subsets(lo):
+			sum = DIMKER[s]
+			for j in s:
+				sum += K[pi[lo.index(j)]]
+			if K[i] <= sum:
+				Test = Test
+			else:
+				Test = False
+	return Test
+def SUM(q):
+	S= 0
+	for k in range(len(q)):
+		S += q[k]
+	return S
 
-
-
-
+def dfs(p,LE,NN,P,DIMKER):
+	Hwvs = []
+	q = [1]
+	q.extend([0]*(len(LE)-1))
+	Stack = deque([q])
+	while len(Stack) != 0:
+		s = Stack.pop()
+		if SUM(s) == p:
+			Hwvs.append(s)
+		else:
+			i = [index for index,item in enumerate(s) if item != 0][-1]
+			parents = P.upper_covers(LE[i])
+			par1 = []
+			for q in parents:
+				par1.extend(P.lower_covers(q))
+			par1 = list(set(par1))
+			cci = [LE.index(q) for q in par1 if LE.index(q) >= i]
+			child = P.lower_covers(LE[i])
+			ci = [LE.index(c) for c in child]
+			ci.extend(cci)
+			if len(ci) != 0:
+				for k in ci:
+					r = s[:]
+					r[k] += 1
+					if (r[k] <= NN[k]) and (cond(r,k,LE,P,DIMKER) == True):
+						Stack.append(r)
+	return Hwvs
+ 
 
 
 '''
@@ -92,5 +142,24 @@ def S2AB(E,m):
 		D1.extend([d]*m)
 	M = csr_matrix((D1,(I1,J1)),shape=(p*m,q*m))
 	return M
+#A is csr_matrix, removes zero rows and zero columns
+def trim(A):
+	B = A[A.getnnz(1)>0][:,A.getnnz(0)>0]
+	return B
+
+#A is matrix over a polynomial ring
+def minRK(A):
+	m = min(A.nrows(),A.ncols())
+	k = 1
+	while k <= m:
+		I = ideal(A.minors(k)).groebner_basis()
+		if I != [1]:
+			break
+		k += 1
+	return k-1
+
+
+
+
 
 
