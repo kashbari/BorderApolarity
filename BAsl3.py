094af437ee38de80f48ab1b2180d922f4d9f7125
kashbari <admin@kashbari.com>
2019-08-06 18:49:59 -0500

    further modified BAsl3.py

--- BAsl3.py
+++ BAsl3.py
@@ -107,7 +107,7 @@ def AnnPlane(c):
 		H11.append(None)
 	c1 = map(str,c)
 	c1 = ''.join(c1)
-	with open("sl3rk{}.txt".format(c1),'w' as ff:
+	with open("sl3rk{}.txt".format(c1),'w') as ff:
 		ff.write('c is'+str(c)+'\n')
 		for z in itertools.product(H22,H30,H03,H00,H11):
 			K = []
@@ -207,8 +207,6 @@ for c in C:
 
 # Main Code to run
 import multiprocessing as mp
-
-pool = mp.Pool(mp.cpu_count())
 
 def main():
 	pool = mp.Pool(mp.cpu_count())
